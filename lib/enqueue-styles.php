3dbe40bdf944c82a052a14d8751d102f251fab45
Michael Kidby <michaelkidby@Michaels-MacBook-Pro.local>
2021-12-28 18:33:03 -0500

    adding time function to remove cacheing

--- lib/enqueue-styles.php
+++ lib/enqueue-styles.php
@@ -2,7 +2,7 @@
 
 
 function millmountain_assets() {
-	wp_enqueue_style( 'millmountain-stylesheet', get_template_directory_uri() . '/assets/css/main.css', array(), '1.0.0', 'all' );
+	wp_enqueue_style( 'millmountain-stylesheet', get_template_directory_uri() . '/assets/css/main.css', array(), time(), 'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'millmountain_assets' );
