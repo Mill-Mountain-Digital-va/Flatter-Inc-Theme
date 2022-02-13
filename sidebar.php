5f093826c5b28fd399c90cea84a1ac985d451a27
Michael Kidby <michaelkidby@Michaels-MacBook-Pro-2.local>
2022-02-13 16:47:55 -0500

    color changes

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@ function millmountain_sidebar_widgets() {
 	register_sidebar(
 		array(
 			'id'            => 'primary-sidebar',
-			'name'          => esc_html__( 'Primary Sidebar', millmountain ),
+			'name'          => esc_html__( 'Primary Sidebar', 'millmountain' ),
 			'description'   => esc_html__( 'description' ),
 			'before_widget' => '<section id="%1$s" class="" >',
 			'after_widget'  => '</section>',
