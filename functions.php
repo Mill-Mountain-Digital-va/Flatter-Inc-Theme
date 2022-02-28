609bce40fca066a603650ca0b29bcf20a1d46ed3
Michael Kidby <kidbymichaelb@gmail.com>
2022-02-27 22:58:56 -0500

    contact php form

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 require_once( 'lib/helpers.php' );
 require_once( 'lib/sidebars.php' );
 require_once( 'lib/enqueue-styles.php' );
-// require_once( 'lib/contact-form.php' );
+require_once( 'lib/contact-form.php' );
 require_once( 'lib/customizer.php' );
 require_once( 'lib/nav-menus.php' );
 require_once( 'lib/javascript.php' );
