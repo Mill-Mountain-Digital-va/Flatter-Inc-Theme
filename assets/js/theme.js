64d1cfd75f0cee418a54bb5bac3bbaea096cca1c
Michael Kidby <kidbymichaelb@gmail.com>
2022-03-24 22:31:27 -0400

    update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -102,7 +102,7 @@ accordionParents.forEach((item,index) => {
         if(scrollIndex === 0){
             rightArrow.classList.remove('u-none');
             leftArrow.classList.add('u-none');
-        } else if(scrollIndex >= accordionSub[index].scrollLeftMax - 20){
+        } else if(scrollIndex >= accordionSub[index].scrollLeftMax - 80){
             leftArrow.classList.remove('u-none');
             rightArrow.classList.add('u-none');
 
