af111164a8d6a28a78dd486c49b3fa9ef441ffdb
Michael Kidby <kidbymichaelb@gmail.com>
2022-03-27 19:10:24 -0400

    update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -18,8 +18,11 @@ let customLogo = document.querySelector(".custom-logo-link");
 const dropDownButtons = document.querySelectorAll(".dropdown-button");
 const hiddenContentArray = document.querySelectorAll(".mill-mountain-tab-hidden-content");
 const dropdownTextArray = document.querySelectorAll('.mill-mountain-tab-button p');
-const dropdownTextWithButton = Array.from(dropdownTextArray).map(text => text.previousElementSibling.classList['value'] === 'dropdown-button');
+const dropdownTextWithButton = Array.from(dropdownTextArray).filter(previousEle());
 
+function previousEle(item){
+    item.previousElementSibling.classList['value'] === 'dropdown-button';
+}
 
 dropDownButtons.forEach((item, index) => {
     item.addEventListener('click', (e) => {
