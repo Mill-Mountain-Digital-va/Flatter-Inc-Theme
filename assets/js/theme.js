2e8baf9117447d9365178f8ef33debe3dbe848cf
Michael Kidby <kidbymichaelb@gmail.com>
2022-03-27 17:03:41 -0400

    update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -21,7 +21,7 @@ const hiddenContentArray = document.querySelectorAll(".mill-mountain-tab-hidden-
 
 dropDownButtons.forEach((item, index) => {
     item.addEventListener('click', (e) => {
-        hiddenContentArray[index].classList.toggle('u-none');
+        hiddenContentArray[index].classList.toggle('u-none slidedown');
     })
 })
 
