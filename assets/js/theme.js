6275dfde47be4db932a6a92670660d69a3a55952
mikekidby <mkidby@wiley.com>
2022-04-06 22:37:35 -0400

    updating slider function values

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -156,7 +156,7 @@ accordionParents.forEach((item,index) => {
     rightArrow.addEventListener('click', (e) => {
         
         // accordionSub[index].scrollBy(400,0);
-        sideScroll(accordionSub[index], 'right', 100, 400, 3)
+        sideScroll(accordionSub[index], 'right', 5, 400, 10)
      
     });
 
@@ -166,7 +166,7 @@ accordionParents.forEach((item,index) => {
     leftArrow.addEventListener('click', (e) => {
         
         // accordionSub[index].scrollBy(-400,0);  
-        sideScroll(accordionSub[index], 'left', 20, 400, 5)
+        sideScroll(accordionSub[index], 'left', 5, 400, 10)
     
     });
 
